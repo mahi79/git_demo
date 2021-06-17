ba96787ee25de23a001586a0021e3900bbe038f6
Maheswar Reddy <vmsmrn@gmail.com>
2021-06-17 08:46:57 +0530

    Adding more commits using single command

--- first_python.py
+++ first_python.py
@@ -3,3 +3,6 @@ print('Welcome to first python file in repo')
 print('This is first line before doing first commit')
 
 print('This is second line after first commit')
+
+
+print('This is third line added after second commit')
